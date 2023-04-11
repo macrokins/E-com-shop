141a2952dcbdd6c28701891bf47642890641be4c
ijakpa <missdoraty@gmail.com>
2023-04-11 11:50:42 +0100

    linked all css files to a single file

--- shop/script.js
+++ shop/script.js
@@ -1,4 +1,3 @@
-
 // -----Hamburger----
 
 const navSlide = () => {
@@ -25,3 +24,26 @@ const navSlide = () => {
   };
 };
 window.onload = () => navSlide();
+
+
+// ----Body section-1 image slideshow-----
+let slideIndex = 0;
+showSlides ();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++; 
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  
+}
